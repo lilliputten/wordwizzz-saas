eb68993f7f7c30e175002016f245aeb26c79d665
lilliputten <lilliputten@gmail.com>
2024-11-05 01:55:11 +0300

    Issue #3: Fixed another nextjs build bug (dynamic code include in `react-email`).

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
 export { auth as middleware } from '@/auth';
+
+export const config = {
+  matcher: '/',
+  unstable_allowDynamic: [
+    // prettier-ignore
+    "**/node_modules/@react-email*/**/*.mjs*",
+  ],
+};
